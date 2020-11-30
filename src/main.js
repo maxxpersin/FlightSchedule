ec12ae4ce00fd3a0f8815174569d352d8ccd2155
Maxx Persin <maxxpersin@Maxxs-MacBook-Pro.local>
2020-11-30 14:02:12 -0600

    boilerplate

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+var clock, departureBody, arrivalBody;
+
 function init() {
-    
+    clock = document.getElementById('clock');
+    setInterval(renderTime, 1000);
+
+    departureBody = document.getElementById('departures-body');
+    arrivalBody = document.getElementById('arrivals-body');
+}
+
+function onTimeChange(time) { //Does all my updating
+
+}
+
+function renderTime() {
+    let time = new Date();
+    clock.textContent = time.toLocaleString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false
+    });
+
 }
