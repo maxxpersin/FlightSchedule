fc3af4eb960ffa2359e2b1af284fb7fb892a8697
Maxx Persin <maxxpersin@Maxxs-MacBook-Pro.local>
2020-11-30 17:28:48 -0600

    mockup done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var clock, departureBody, arrivalBody;
+var clock, departureBody, arrivalBody, arrivals = [], departures = [];
 
 function init() {
     clock = document.getElementById('clock');
@@ -6,6 +6,54 @@ function init() {
 
     departureBody = document.getElementById('departures-body');
     arrivalBody = document.getElementById('arrivals-body');
+
+    initFlights();
+}
+
+function initFlights() {
+    for (let i = 0; i < 10; i++) {
+        arrivals.push(new Arrival({
+            airline: 'American Airlines',
+            num: 'A 10',
+            arrivingFrom: 'LAX',
+            schedTime: '10:00',
+            expectedTime: '10:10',
+            gate: 'C19',
+            status: 'On Time'
+        }));
+
+        arrivalBody.appendChild(document.createElement('tr'));
+        arrivalBody.lastChild
+            .innerHTML = `<td>${arrivals[i].airline}</td>
+            <td>${arrivals[i].num}</td>
+            <td>${arrivals[i].arrivingFrom}</td>
+            <td>${arrivals[i].schedTime}</td>
+            <td>${arrivals[i].expectedTime}</td>
+            <td>${arrivals[i].gate}</td>
+            <td>${arrivals[i].status}</td>`
+    }
+
+    for (let i = 0; i < 10; i++) {
+        departures.push(new Departure({
+            airline: 'American Airlines',
+            num: 'Z 778',
+            departingTo: 'MKE',
+            schedTime: '15:00',
+            expectedTime: '15:00',
+            gate: 'A9',
+            status: 'On Time'
+        }));
+
+        departureBody.appendChild(document.createElement('tr'));
+        departureBody.lastChild
+            .innerHTML = `<td>${departures[i].airline}</td>
+            <td>${departures[i].num}</td>
+            <td>${departures[i].departingTo}</td>
+            <td>${departures[i].schedTime}</td>
+            <td>${departures[i].expectedTime}</td>
+            <td>${departures[i].gate}</td>
+            <td>${departures[i].status}</td>`
+    }
 }
 
 function onTimeChange(time) { //Does all my updating
@@ -20,5 +68,32 @@ function renderTime() {
         second: 'numeric',
         hour12: false
     });
+}
 
+class Arrival {
+    airline;
+    num;
+    arrivingFrom;
+    schedTime;
+    expectedTime;
+    gate;
+    status;
+
+    constructor(init) {
+        Object.assign(this, init);
+    }
+}
+
+class Departure {
+    airline;
+    num;
+    departingTo;
+    schedTime;
+    expectedTime;
+    gate;
+    status;
+
+    constructor(init) {
+        Object.assign(this, init);
+    }
 }
